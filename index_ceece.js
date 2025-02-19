1e05a19f16771426b9dcb1dea5ef4c8a7c9b235f
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2025-02-19 10:06:25 -0300

    Update index_ceece.js

--- index_ceece.js
+++ index_ceece.js
@@ -121,7 +121,7 @@ res.send(`
 });
 
 // Inicia o servidor
-app.listen(port, '92.112.179.191', () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor rodando em http://92.112.179.191:${port}`);
 });
 
