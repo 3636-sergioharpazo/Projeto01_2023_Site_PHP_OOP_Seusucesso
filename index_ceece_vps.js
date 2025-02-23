0ecc59c0a9ef9f3dc762c47e7b5c5dd772ec70e1
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2025-02-23 19:09:34 -0300

    Update index_ceece_vps.js

--- index_ceece_vps.js
+++ index_ceece_vps.js
@@ -178,32 +178,7 @@ client.on('message', async (msg) => {
     await delay(2000);
     client.sendMessage(msg.from, `Desculpe, ${nomeCliente.split(" ")[0]}, não entendi sua mensagem. Tente digitar 'menu', 'oi' ou outra opção.`);
   }
-});
-// Função para criar delay
-const delay = ms => new Promise(res => setTimeout(res, ms));
-  // **Registrar evento de mensagem após a inicialização do cliente**
-  client.on('message', async (msg) => {
-    const chat = await msg.getChat();
-    const contact = await msg.getContact();
-    const nomeCliente = contact.pushname || "Cliente";
 
-    // Mensagem de boas-vindas e menu principal
-    if (/^(menu|oi|Oi|ol[áa]|boa noite|bom dia)$/i.test(msg.body)) {
-      await chat.sendStateTyping();
-      await delay(2000);
-
-      axios.get('https://ceecegril.antoniooliveira.shop/menus_bot.php?action=menu', {
-        timeout: 10000 // 10 segundos de timeout
-      })
-      .then(response => {
-        client.sendMessage(msg.from, `Olá, ${nomeCliente.split(" ")[0]}! 👋\n\n${response.data}`);
-      })
-      .catch(error => {
-        console.error("Erro ao obter menu:", error);
-        client.sendMessage(msg.from, "Desculpe, não foi possível obter o menu no momento. Tente novamente mais tarde.");
-      });
-    }
-  
 
 // Função para criar delay
 const delay = ms => new Promise(res => setTimeout(res, ms));
