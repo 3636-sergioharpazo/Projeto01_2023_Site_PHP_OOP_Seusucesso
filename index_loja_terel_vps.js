4047e49dd0f667e56ef2726b8be07fd993246e67
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2025-02-24 15:09:45 -0300

    Update index_loja_terel_vps.js

--- index_loja_terel_vps.js
+++ index_loja_terel_vps.js
@@ -7,6 +7,7 @@ const express = require('express');
 const axios = require('axios');
 const { exec } = require('child_process');
 const app = express();
+
 const PORT = 3003;
 const qrCodeDir = '/var/www/html/bot2';  // Diretório onde o QR será salvo
 
