b2e95a6256c4759819616f1e0fb3a72e59ca3ea7
Antonio Oliveira - DEV <63135415+3636-sergioharpazo@users.noreply.github.com>
2025-03-02 13:20:32 -0300

    Update index_ceece_vps.js

--- index_ceece_vps.js
+++ index_ceece_vps.js
@@ -9,6 +9,7 @@ const { exec } = require('child_process');
 
 const app = express();
 const PORT = 3002;
+
 const qrCodeDir = '/var/www/html';  // Diretório onde o QR será salvo
 
 let isQRCodeGenerated = false; // Controle para evitar a repetição do QR Code
